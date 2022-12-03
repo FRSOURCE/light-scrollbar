d707bd15facdb5df2633b00dbf4967db0a9436b3
Szymon-dziewonski <Szymon-dziewonski@users.noreply.github.com>
2022-12-03 13:59:26 +0000

    deploy: 25d11b08347317f736f954c79a56f4dcc1cb6d32

---
+++ assets/api.md.f78deb14.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,o as t,c as e,a as r}from"./app.51825060.js";const u=JSON.parse('{"title":"Api","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","lastUpdated":1670075834000}'),i={name:"api.md"},l=r("",2),c=[l];function s(h,o,_,n,p,d){return t(),e("div",null,c)}const f=a(i,[["render",s]]);export{u as __pageData,f as default};
